565af5b077cd57d60059b0cd6dc9c30473d0f845
Julien Eudine <julien@eudine.fr>
2021-03-16 18:51:46 +0100

    Xor gate added

--- src/gate.rs
+++ src/gate.rs
@@ -1,6 +1,6 @@
 use crate::interface::Channel;
 use std::fmt::Display;
-use std::ops::{BitAnd, BitOr};
+use std::ops::{BitAnd, BitOr, BitXor};
 
 struct And<'a, T: Copy + PartialEq + Display + BitAnd + BitAnd<Output = T>> {
     in1: &'a dyn Channel<T>,
@@ -8,6 +8,11 @@ struct And<'a, T: Copy + PartialEq + Display + BitAnd + BitAnd<Output = T>> {
 }
 
 struct Or<'a, T: Copy + PartialEq + Display + BitOr + BitOr<Output = T>> {
+    in1: &'a dyn Channel<T>,
+    in2: &'a dyn Channel<T>,
+}
+
+struct Xor<'a, T: Copy + PartialEq + Display + BitXor + BitXor<Output = T>> {
     in1: &'a dyn Channel<T>,
     in2: &'a dyn Channel<T>,
 }
@@ -27,5 +32,14 @@ where
 {
     fn read(&self) -> T {
         self.in1.read() | self.in2.read()
+    }
+}
+
+impl<'a, T> Channel<T> for Xor<'a, T>
+where
+    T: Copy + PartialEq + Display + BitXor + BitXor<Output = T>,
+{
+    fn read(&self) -> T {
+        self.in1.read() ^ self.in2.read()
     }
 }
