1bfbfebd826f6ec1f0e07d38078b6284232959a1
Julien Eudine <julien@eudine.fr>
2021-04-20 09:59:46 +0200

    doc written

--- src/pub_interface.rs
+++ src/pub_interface.rs
@@ -1,5 +1,9 @@
 use crate::interface::TChannel;
 
+///Each node of a circuit can be represented by a variable implementing the [`Channel<T>`] trait (with `T`, the actual type of the circuit node).
+///
+///This trait is hidden by the library and you usually don't have to think about it.
 pub trait Channel<T: TChannel> {
+    /// Return the value of the circuit node
     fn read(&self) -> T;
 }
